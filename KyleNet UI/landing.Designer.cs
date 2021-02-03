9e1a94634cd196b06b8ac22a580ca9ccfa9939f4
Kyle <ixkalz@gmail.com>
2021-02-03 12:44:35 +0000

    Majority of UI functionality completed

--- KyleNet UI/landing.Designer.cs
+++ KyleNet UI/landing.Designer.cs
@@ -29,7 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.landingProgressBar = new System.Windows.Forms.ProgressBar();
+            this.gatheringLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -43,35 +44,49 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Drag and drop or upload an image";
             // 
-            // button1
+            // landingProgressBar
             // 
-            this.button1.Location = new System.Drawing.Point(579, 218);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.landingProgressBar.Location = new System.Drawing.Point(140, 301);
+            this.landingProgressBar.Name = "landingProgressBar";
+            this.landingProgressBar.Size = new System.Drawing.Size(426, 23);
+            this.landingProgressBar.TabIndex = 1;
+            this.landingProgressBar.Visible = false;
+            // 
+            // gatheringLabel
+            // 
+            this.gatheringLabel.AutoSize = true;
+            this.gatheringLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gatheringLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.gatheringLabel.Location = new System.Drawing.Point(287, 281);
+            this.gatheringLabel.Name = "gatheringLabel";
+            this.gatheringLabel.Size = new System.Drawing.Size(130, 17);
+            this.gatheringLabel.TabIndex = 2;
+            this.gatheringLabel.Text = "Gathering results ...";
+            this.gatheringLabel.Visible = false;
             // 
             // landing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(692, 336);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gatheringLabel);
+            this.Controls.Add(this.landingProgressBar);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "landing";
             this.Text = "KyleNet UI";
-            this.Load += new System.EventHandler(this.landing_Load);
+            this.Load += new System.EventHandler(this.landing_LoadAsync);
+            this.Click += new System.EventHandler(this.landing_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar landingProgressBar;
+        private System.Windows.Forms.Label gatheringLabel;
     }
 }
 
--- KyleNet UI/results.cs
+++ KyleNet UI/results.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +13,26 @@ namespace KyleNet_UI
 {
     public partial class results : Form
     {
-        public results()
+        public results(string image, Bitmap gradcam, string prediction)
         {
             InitializeComponent();
+            originalImageBox.Image = new Bitmap(image);
+            xaiImageBox.Image = gradcam;
+            predictionLabel.Text = prediction;
+
+            Color color = prediction == "COVID-19" ? Color.Red : Color.SeaGreen;
+            predictionLabel.ForeColor = color;
+
+            resultListBox.Items.Add(Path.GetFileName(image));
         }
 
         private void results_Load(object sender, EventArgs e)
         {
             Text = "KyleNet UI";
-
-            
+            MaximumSize = new Size(1006, 633);
         }
 
-        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -39,7 +44,18 @@ namespace KyleNet_UI
 
         private void closeToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Are you sure you want to exit?", "Exiting", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            Hide();
+            landing _landing = new landing();
+            _landing.Closed += (s, args) => Close();
+            _landing.ShowDialog();
+        }
+
+        private void closeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit?", "Exiting", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            
+            if (result == DialogResult.Yes)
+                Application.Exit();
         }
     }
 }
